b5bec6bd19a64f5f0ece891902024f82a6a4d13d
Alomgir hasan Shakib <alomgirhasanshakib@gmail.com>
2024-04-04 10:02:07 +0600

    comnplete the header section design

--- src/Pages/Shared/Header/Header.jsx
+++ src/Pages/Shared/Header/Header.jsx
@@ -3,10 +3,16 @@ import moment from "moment";
 
 const Header = () => {
   return (
-    <div>
-      <img src={logo} alt="" />
-      <p className="text-2xl">Journalism Without Fear or Favour</p>
-      <p className="text-2xl">{moment().format("dddd, MMMM D, YYYY")}</p>
+    <div className="text-center mt-12 space-y-2">
+      <div className="flex justify-center">
+        <img src={logo} alt="" />
+      </div>
+      <p className="text-2xl text-[#706F6F] font-medium">
+        Journalism Without Fear or Favour
+      </p>
+      <p className="text-2xl text-[#706F6F] font-medium">
+        {moment().format("dddd, MMMM D, YYYY")}
+      </p>
     </div>
   );
 };
